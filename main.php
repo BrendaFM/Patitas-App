c3518ad2a14ae12b0c9a954473655cbfd5d2e390
Luis <luisbo_56@hotmail.com>
2022-03-29 20:01:56 -0500

    correccion a errores en main.php y view-info-albergue

--- main.php
+++ main.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="es">
 
@@ -8,17 +9,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link type="image/png" rel="icon" sizes="32x32" href="img/huella">
-    <title>Patitas App</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <!-- Custom fonts for this template-->    
-    <!-- <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"> -->
 
-    <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <title>RECORSEN</title>
 
     <!-- Font-Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css"/>
+    
+    <!-- Custom fonts for this template-->
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link type="image/png" rel="icon" sizes="32x32" href="img/logo.jpg">
+
+    <!-- DATA TABLE -->
+    <link href="//cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/buttons/2.0.1/css/buttons.dataTables.min.css" rel="stylesheet">
 
 </head>
 
@@ -28,12 +36,11 @@
     <div id="wrapper">
 
         <!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
+        <ul class="navbar-nav sidebar sidebar-dark accordion" id="accordionSidebar" style="background-color: #14b3dd;">
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center">
+            <a style="color: white;" class="sidebar-brand d-flex align-items-center justify-content-center">
                 <div class="sidebar-brand-icon rotate-n-15">
-                <i class="fa-solid fa-paw"></i>
+                    <i class="fa-solid fa-paw"></i>
                 </div>
                 <div class="sidebar-brand-text mx-3">Patitas App</div>
             </a>
@@ -56,15 +63,18 @@
                 Mascotas
             </div>
 
-            <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item active">
+            <!-- Divider -->
+            <!-- <hr class="sidebar-divider"> -->
+
+            <!-- Nav Item -->
+            
+            <li class="nav-item">
                 <a class="nav-link" href="main.php?view=filtro-mascotas">
                 <i class="fa-solid fa-filter"></i>
                     <span>Filtro de Mascotas</span></a>
             </li>
-
-            <!-- Nav Item - Utilities Collapse Menu -->
-            <li class="nav-item active">
+            
+            <li class="nav-item">
                 <a class="nav-link" href="main.php?view=mascotas-adoptadas">
                 <i class="fa-solid fa-house-chimney"></i>
                     <span>Mascotas Adoptadas</span></a>
@@ -79,7 +89,7 @@
             </div>
 
             <!-- Nav Item - Charts -->
-            <li class="nav-item active">
+            <li class="nav-item">
                 <a class="nav-link" href="main.php?view=comentarios">
                     <i class="fa-solid fa-comments"></i>
                     <span>Comentarios</span></a>
@@ -88,9 +98,9 @@
             <!-- Divider -->
             <hr class="sidebar-divider d-none d-md-block">
 
-            <li class="nav-item active">
+            <li class="nav-item">
                 <a class="nav-link" href="main.php?view=info-albergue">
-                    <i class="fa-solid fa-comments"></i>
+                    <i class="fa-solid fa-circle-info"></i>
                     <span>Acerca del albergue</span></a>
             </li>
 
@@ -98,34 +108,19 @@
             <div class="text-center d-none d-md-inline">
                 <button class="rounded-circle border-0" id="sidebarToggle"></button>
             </div>
-
-            <!-- Sidebar Message -->
-            <!--
-            <div class="sidebar-card d-none d-lg-flex">
-                <img class="sidebar-card-illustration mb-2" src="img/undraw_rocket.svg" alt="...">
-                <p class="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components, and more!</p>
-                <a class="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to Pro!</a>
-            </div>
-            -->
-
         </ul>
-         <!--End of Sidebar -->
 
-
-        <!-- Content Wrapper -->
         <div id="content-wrapper" class="d-flex flex-column">
 
-            <!-- Main Content -->
             <div id="content">
 
                 <!-- Topbar -->
                 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
-                    <!-- Topbar Navbar -->
-                    <ul class="navbar-nav ml-auto">
-
-
-                    </ul>
+                    <!-- Sidebar Toggle (Topbar) -->
+                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                        <i class="fa fa-bars"></i>
+                    </button>
 
                 </nav>
                 <!-- End of Topbar -->
@@ -133,13 +128,8 @@
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
 
-                    <!-- Page Heading -->
-                    <!-- <div class="d-sm-flex align-items-center justify-content-between mb-4"> -->
-                    <!-- </div> -->
-
-                    <!-- aqui se cargara el contenido de forma asincrona -->
+                    <!-- Contenido de la pagina -->
                     <div id="contenido">
-
 
                     </div>
                 </div>
@@ -151,13 +141,8 @@
             <!-- Footer -->
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
-                    <div class="row">
-                        <div class="col">
-                            <div class="float-left">Contactos:</div>
-                            <div class="float-right">Apoyo-Donaciones</div><br>
-                            <div class="float-left">9123481724 - 128738124</div>
-                            <div class="float-right">patitasapp@gmail.com</div><br>
-                        </div>
+                    <div class="copyright text-center my-auto">
+                        <span>RECORSEN &copy; Ingeniería de Software con Inteligencia Artificial - IV</span>
                     </div>
                 </div>
             </footer>
@@ -169,11 +154,18 @@
     </div>
     <!-- End of Page Wrapper -->
 
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+
     <!-- Bootstrap core JavaScript-->
     <script src="libs/jquery/jquery.min.js"></script>
+    <script src="libs/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <!-- <script src="libs/jquery-easing/jquery.easing.min.js"></script> -->
+    <script src="libs/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
     <script src="js/sb-admin-2.min.js"></script>
@@ -181,19 +173,29 @@
     <!-- Page level plugins -->
     <script src="libs/chart.js/Chart.min.js"></script>
 
-    <!-- Invocar a la libreria de navegacion -->
+    <!-- Page level custom scripts -->
     <script src="js/loadweb.js"></script>
+    <script src="libs/jquery-mask/jquery.mask.min.js"></script>
+    <!-- data table-->
+    <script src="//cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.0.1/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.print.min.js"></script>
+
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/chartjs-plugin-datalabels/2.0.0/chartjs-plugin-datalabels.min.js" integrity="sha512-R/QOHLpV1Ggq22vfDAWYOaMd5RopHrJNMxi8/lJu8Oihwi4Ho4BRFeiMiCefn9rasajKjnx9/fTQ/xkWnkDACg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    
     <script>
         $(document).ready(function(){
-            //JQuery activado...
             var view = getParam("view");
             if (view != false)
-            $("#contenido").load(`views/${view}.php`);
+                $("#contenido").load(`views/${view}.php`);
             else
-            $("#contenido").load(`views/home.php`);
+                $("#contenido").load(`views/home.php`);
         });
     </script>
 </body>
-
 </html>
